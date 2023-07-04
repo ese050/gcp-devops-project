b39f4b791d7af6064ab53f07d72c33530ae968c6
mrpeadevops <ekrebee@gmail.com>
2023-07-04 08:28:27 -0700

    python centerize ccs code flask01

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, simple Flask application'
-
+    return '''
+    <div style="text-align: center;">
+        <h1 style="text-transform: uppercase; font-weight: bold;">Hello, From Ese</h1>
+        <p>Welcome to my DevOps Flask app</p>
+    </div>
+    '''
 if __name__ == '__main__':
     app.run()
